1bbadb95826bf07882b608e4ee9c176ab8f705d3
Kevin Lin <kevin.shuminglin@gmail.com>
2023-03-26 20:17:56 -0400

    update list user in contact

--- src/main/java/com/d5assignment3506/localmessagingsystem/controllers/ChatController.java
+++ src/main/java/com/d5assignment3506/localmessagingsystem/controllers/ChatController.java
@@ -1,15 +1,25 @@
 package com.d5assignment3506.localmessagingsystem.controllers;
 
+import com.d5assignment3506.localmessagingsystem.entity.User;
+import com.d5assignment3506.localmessagingsystem.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class ChatController {
 
+    @Autowired
+    private UserRepository userRepo;
+
     @RequestMapping(value = "/chat")
-    public String register(Model model3) {
-        model3.addAttribute("title", "register Page");
+    public String register(Model model) {
+
+        List<User> listUsers = userRepo.findAll();
+        model.addAttribute("allUsers", listUsers);
         return "chat";
     }
     
--- src/main/java/com/d5assignment3506/localmessagingsystem/controllers/ContactController.java
+++ src/main/java/com/d5assignment3506/localmessagingsystem/controllers/ContactController.java
@@ -2,16 +2,39 @@
 
 package com.d5assignment3506.localmessagingsystem.controllers;
 
+import com.d5assignment3506.localmessagingsystem.entity.User;
+import com.d5assignment3506.localmessagingsystem.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class ContactController {
 
+    @Autowired
+    private UserRepository userRepo;
+
     @RequestMapping(value = "/contact")
-    public String register(Model model3) {
-        model3.addAttribute("title", "contact Page");
+    public String register(Model model) {
+
+        List<User> listUsers = userRepo.findAll();
+        model.addAttribute("allUsers", listUsers);
+
+        return "contact";
+    }
+
+    @PostMapping("/userDetail")
+    public String userDetails(@ModelAttribute("user") String username, Model model) {
+
+        User userDetail = userRepo.findByUsername(username);
+
+        model.addAttribute("userDetail", userDetail);
+
         return "contact";
     }
     
