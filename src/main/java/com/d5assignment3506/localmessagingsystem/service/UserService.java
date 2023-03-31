018619b75a898619e5b41060f6e55c7a76b60985
shijiealgoma <ssun@algomau.ca>
2023-03-31 19:53:21 -0400

    update

--- src/main/java/com/d5assignment3506/localmessagingsystem/service/UserService.java
+++ src/main/java/com/d5assignment3506/localmessagingsystem/service/UserService.java
@@ -1,4 +1,13 @@
 package com.d5assignment3506.localmessagingsystem.service;
 
+import com.d5assignment3506.localmessagingsystem.entity.User;
+
 public class UserService {
+
+    public void save(User user) {
+    }
+
+    public User get(Long id) {
+        return null;
+    }
 }
